7ff3d5739c85e7c4512f6fa8bb130c922099a92e
Kutay Yilmaz <89530848+yilmazkutay@users.noreply.github.com>
2026-04-02 22:32:59 +0300

    Create Test.java

---
+++ OOP/Private/Test.java
@@ -0,0 +1,11 @@
+package OOP.privateoop;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Car car = new Car();
+
+        car.setBrand("Audi");
+        System.out.print("The Brand is: " + car.getBrand());
+    }
+}
